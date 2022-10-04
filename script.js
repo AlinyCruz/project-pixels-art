006c063a026196f99b31c9c2931c8d5d60845abe
Aline Oliveira Cruz <aliny.cruz@yahoo.com.br>
2022-10-04 16:34:17 -0300

    Requisito 1, 2, e 3.

---
+++ script.js
@@ -0,0 +1,30 @@
+// Adicione à página o título "Paleta de Cores".
+const elementoH1 = document.createElement('h1');
+elementoH1.innerText = 'Paleta de Cores';
+elementoH1.id = 'title';
+document.body.appendChild(elementoH1);
+
+// Criando caixa pai.
+const main = document.createElement('main');
+main.id = 'color-palette';
+document.body.appendChild(main);
+
+// Criando caixas filhas.
+for (let i = 1; i <= 4; i += 1) {
+    const divsMain = document.createElement('div');
+    main.appendChild(divsMain);
+    divsMain.className = 'color';
+}
+
+// Capturando as cores da paleta.
+const divPreta = document.getElementsByClassName('color')[0];
+const div2 = document.getElementsByClassName('color')[1];
+const div3 = document.getElementsByClassName('color')[2];
+const div4 = document.getElementsByClassName('color')[3];
+
+divPreta.style.background = 'black';
+div2.style.background = 'yellow';
+div3.style.background = 'red';
+div4.style.background = 'blue';
+
+
