123f57eb7025ef6fc4beee80f7560934e1998f3a
Aline Oliveira Cruz <aliny.cruz@yahoo.com.br>
2022-10-04 19:42:25 -0300

    Requisito 4.

--- script.js
+++ script.js
@@ -22,9 +22,22 @@ const div2 = document.getElementsByClassName('color')[1];
 const div3 = document.getElementsByClassName('color')[2];
 const div4 = document.getElementsByClassName('color')[3];
 
+// Dando cores a paleta.
 divPreta.style.background = 'black';
 div2.style.background = 'yellow';
 div3.style.background = 'red';
 div4.style.background = 'blue';
 
+const buttonAleatorio = document.getElementById('button-random-color');
 
+buttonAleatorio.addEventListener('click', coresAleatorias);
+
+function coresAleatorias() {
+    let color1 = Math.floor(Math.random() * 2000);
+    let color2 = Math.floor(Math.random() * 3000);
+    let color3 = Math.floor(Math.random() * 4000);
+    
+    div2.style.backgroundColor = `#${color1}`;
+    div3.style.backgroundColor = `#${color2}`;
+    div4.style.backgroundColor = `#${color3}`;
+}
