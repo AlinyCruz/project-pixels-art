da521d68780d56a7909ee1957cd979b8db46f947
Aline Oliveira Cruz <aliny.cruz@yahoo.com.br>
2022-10-05 21:41:27 -0300

    Requisito 6 e 7.

--- script.js
+++ script.js
@@ -28,16 +28,75 @@ div2.style.background = 'yellow';
 div3.style.background = 'red';
 div4.style.background = 'blue';
 
+// Capturando botão de cores aleatorias.
 const buttonAleatorio = document.getElementById('button-random-color');
 
+// Adicionando evento ao clicar o botão.
 buttonAleatorio.addEventListener('click', coresAleatorias);
 
+// Elaborando função para gerar cores aleatórias ao clicar o botão.
+// if (localStorage.corAleatoria1) {
+//     div2.value = localStorage.corAleatoria1;
+// }
+
+// if (localStorage.corAleatoria2) {
+//     div3.value = localStorage.corAleatoria2;
+// }
+
+// if (localStorage.corAleatoria3) {
+//     div4.value = localStorage.corAleatoria3;
+// }
+
 function coresAleatorias() {
-    let color1 = Math.floor(Math.random() * 2000);
-    let color2 = Math.floor(Math.random() * 3000);
-    let color3 = Math.floor(Math.random() * 4000);
-    
+
+    let color1 = Math.floor(Math.random() * 350);
+    let color2 = Math.floor(Math.random() * 450);
+    let color3 = Math.floor(Math.random() * 550);
+
     div2.style.backgroundColor = `#${color1}`;
     div3.style.backgroundColor = `#${color2}`;
     div4.style.backgroundColor = `#${color3}`;
+
+    localStorage.setItem('corAleatoria1', `#${color1}`);
+    localStorage.setItem('corAleatoria2', `#${color2}`);
+    localStorage.setItem('corAleatoria3', `#${color3}`);
+}
+
+// if(localStorage.cor1) {
+//     div2.value = localStorage.cor1;
+// }
+// if(localStorage.cor1) {
+//     div3.value = localStorage.cor2;
+// }
+// if(localStorage.cor1) {
+//     div4.value = localStorage.cor3;
+// }
+
+// const salvaCores = function() {
+//     let cor1 = div2.value;
+//     let cor2 = div3.value;
+//     let cor3 = div4.value;
+
+//     localStorage.setItem('cor1', cor1);
+//     localStorage.setItem('cor2', cor2);
+//     localStorage.setItem('cor3', cor3);
+
+// }
+
+// document.onchange = salvaCores;
+
+// Criando caixa pai.
+
+const article = document.createElement('article');
+article.id = 'pixel-board';
+document.body.appendChild(article);
+
+// Criando caixas filhas.
+
+for (let i = 1; i <= 5; i += 1) {
+    for (let i = 1; i <= 5; i += 1) {
+        let divCores = document.createElement('div');
+        article.appendChild(divCores);
+        divCores.className = 'pixel';
+    }
 }
